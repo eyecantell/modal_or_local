aeb48bb2d454ecd325388b591349186a18276240
Paul <paul@nowfree.org>
2024-07-15 18:01:26 +0000

    changed mvol to mocal for clarity

--- tests/test_modal_or_local.py
+++ tests/test_modal_or_local.py
@@ -13,9 +13,9 @@ app = modal.App("test_modal_or_local")
 
 MODAL_VOLUME_NAME = "test_modal_or_local_volume"
 MODAL_VOLUME_MOUNT_DIR = "/test_mnt_dir"
-mvol = ModalOrLocal(volume_name=MODAL_VOLUME_NAME, volume_mount_dir = MODAL_VOLUME_MOUNT_DIR)
+mocal = ModalOrLocal(volume_name=MODAL_VOLUME_NAME, volume_mount_dir = MODAL_VOLUME_MOUNT_DIR)
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume}) 
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume}) 
 def test_write_and_read_volume_json_file():
     '''Write a json file to a modal volume then read it (should be able to run both .local() and .remote())'''
 
@@ -24,24 +24,24 @@ def test_write_and_read_volume_json_file():
 
     json_file_full_path = os.path.join(MODAL_VOLUME_MOUNT_DIR, "test_write_and_read_volume_json_file.json")
 
-    mvol.write_json_file(json_file_full_path, test_json_data, force=True)
+    mocal.write_json_file(json_file_full_path, test_json_data, force=True)
 
-    assert mvol.file_or_dir_exists(json_file_full_path)
+    assert mocal.file_or_dir_exists(json_file_full_path)
 
-    read_data = mvol.read_json_file(json_file_full_path)
+    read_data = mocal.read_json_file(json_file_full_path)
     assert read_data is not None
     assert read_data.get('a') == 1
     assert read_data.get('b') == 2
 
     # Remove the test file
-    mvol.remove_file_or_directory(json_file_full_path)
+    mocal.remove_file_or_directory(json_file_full_path)
 
     # Check that file was actually deleted
-    assert not mvol.file_or_dir_exists(json_file_full_path)
+    assert not mocal.file_or_dir_exists(json_file_full_path)
 
     print("Running test_read_file_from_volume", "locally" if modal.is_local() else "remotely", "finished")
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume}) 
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume}) 
 def test_write_and_read_volume_txt_file():
     '''Write a text file to a modal volume then read it (should be able to run both .local() and .remote())'''
 
@@ -50,41 +50,41 @@ def test_write_and_read_volume_txt_file():
     file_full_path = os.path.join(MODAL_VOLUME_MOUNT_DIR, "test_write_and_read_volume_txt_file.txt")
     text_to_encode = "This is some text"
 
-    mvol.write_file(file_full_path, text_to_encode.encode(), force=True)
+    mocal.write_file(file_full_path, text_to_encode.encode(), force=True)
 
-    assert mvol.file_or_dir_exists(file_full_path)
+    assert mocal.file_or_dir_exists(file_full_path)
 
-    content = mvol.read_file(file_full_path)
+    content = mocal.read_file(file_full_path)
     file_text = content.decode('utf-8')
 
     assert file_text is not None
     assert file_text == text_to_encode
 
     # Remove the test file
-    mvol.remove_file_or_directory(file_full_path)
+    mocal.remove_file_or_directory(file_full_path)
 
     # Cannot check that file was actually deleted without a reload of the volume, not sure how to use reload() (get running function error) so commenting out
-    assert not mvol.file_or_dir_exists(file_full_path)
+    assert not mocal.file_or_dir_exists(file_full_path)
 
     print("Running test_write_and_read_volume_txt_file", "locally" if modal.is_local() else "remotely", "finished")
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume})
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume})
 def test_create_or_remove_dir():
     '''Create and remove directory within a volume'''
     for dir_to_create in ["test_create_or_remove_dir_data", "/test_create_or_remove_dir_data/test/a/b/c"]:
         if dir_to_create.startswith("/"): dir_to_create = dir_to_create.replace("/","")
-        dir_to_create_full_path = os.path.join(mvol.volume_mount_dir, dir_to_create)
+        dir_to_create_full_path = os.path.join(mocal.volume_mount_dir, dir_to_create)
         print(f"Creating '{dir_to_create_full_path=}'")
-        mvol.create_directory(dir_to_create_full_path)
-        assert mvol.file_or_dir_exists(dir_to_create_full_path)
-        mvol.remove_file_or_directory(dir_to_create_full_path)
-        assert not mvol.file_or_dir_exists(dir_to_create_full_path)
+        mocal.create_directory(dir_to_create_full_path)
+        assert mocal.file_or_dir_exists(dir_to_create_full_path)
+        mocal.remove_file_or_directory(dir_to_create_full_path)
+        assert not mocal.file_or_dir_exists(dir_to_create_full_path)
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume})
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume})
 def test_listdir():
     '''Create files in a temp directory, then read the list of files in the directory'''
-    temp_dir = os.path.join(mvol.volume_mount_dir, "test_listdir_data")
-    mvol.create_directory(temp_dir)
+    temp_dir = os.path.join(mocal.volume_mount_dir, "test_listdir_data")
+    mocal.create_directory(temp_dir)
 
     # Add some files to the temp directory on the volume
     filenames_created = []
@@ -93,31 +93,31 @@ def test_listdir():
         filenames_created.append(filename)
         full_path = os.path.join(temp_dir, filename)
         file_text = "this is some text in file " + prefix
-        mvol.write_file(full_path, file_text.encode())
+        mocal.write_file(full_path, file_text.encode())
 
     # List the directory and check that all of the expected files are in the list
-    found_filenames = mvol.listdir(temp_dir)
+    found_filenames = mocal.listdir(temp_dir)
     #print(f"{found_filenames=}")
     for filename in filenames_created:
         assert filename in found_filenames, "Expected filename " + filename + " not found in listdir " + str(found_filenames)
 
     # List the directory with full path option and check that all of the expected files are in the list
-    found_filenames_full_path = mvol.listdir(temp_dir, return_full_paths=True)
+    found_filenames_full_path = mocal.listdir(temp_dir, return_full_paths=True)
     #print(f"{found_filenames_full_path=}")
     for filename in filenames_created:
         full_path = os.path.join(temp_dir, filename)
         assert full_path in found_filenames_full_path, "Expected full path " + full_path + " not found in listdir " + str(found_filenames_full_path)
 
     # Remove the temp test dir
-    mvol.remove_file_or_directory(temp_dir)
+    mocal.remove_file_or_directory(temp_dir)
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume})
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume})
 def test_walk():
     '''Create files/dirs in a temp directory, then walk the list of files in the directory'''
-    temp_dir = os.path.join(mvol.volume_mount_dir, "test_walk_data")
-    mvol.create_directory(temp_dir)
+    temp_dir = os.path.join(mocal.volume_mount_dir, "test_walk_data")
+    mocal.create_directory(temp_dir)
     second_level_dir = os.path.join(temp_dir, "mydir")
-    mvol.create_directory(second_level_dir)
+    mocal.create_directory(second_level_dir)
 
     expected_tuples = []
 
@@ -128,7 +128,7 @@ def test_walk():
         filename = prefix + ".txt"
         full_path = os.path.join(temp_dir, filename)
         file_text = "this is some text in file " + prefix
-        mvol.write_file(full_path, file_text.encode())
+        mocal.write_file(full_path, file_text.encode())
         filenames_created.append(filename)
     expected_tuples.append((temp_dir, [os.path.basename(second_level_dir)], filenames_created))
 
@@ -138,12 +138,12 @@ def test_walk():
         filename = prefix + ".txt"
         full_path = os.path.join(second_level_dir, filename)
         file_text = "this is some text in file " + prefix
-        mvol.write_file(full_path, file_text.encode())
+        mocal.write_file(full_path, file_text.encode())
         filenames_created.append(filename)
     expected_tuples.append((second_level_dir, [], filenames_created))
 
     walk_tuples = []
-    for tup in mvol.walk(temp_dir):
+    for tup in mocal.walk(temp_dir):
         walk_tuples.append(tup)
 
     print(f"{expected_tuples=}")
@@ -152,7 +152,7 @@ def test_walk():
     assert walk_tuples_equal(walk_tuples, expected_tuples)
 
     # Remove the temp test dir
-    mvol.remove_file_or_directory(temp_dir)
+    mocal.remove_file_or_directory(temp_dir)
 
 def convert_walk_tuple_lists_to_sets(tuples):
     return [(t[0], frozenset(t[1]), frozenset(t[2])) for t in tuples]
@@ -163,42 +163,42 @@ def walk_tuples_equal(expected, actual) -> bool:
     actual_converted = convert_walk_tuple_lists_to_sets(actual)
     return Counter(expected_converted) == Counter(actual_converted)
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume})
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume})
 def test_get_FileEntry():
     '''Create a file and dir on the volume, check we can get FileEntry of each'''
-    temp_dir = os.path.join(mvol.volume_mount_dir, "test_get_FileEntry", "second_level_dir")
-    mvol.create_directory(temp_dir)
+    temp_dir = os.path.join(mocal.volume_mount_dir, "test_get_FileEntry", "second_level_dir")
+    mocal.create_directory(temp_dir)
     json_file_full_path = os.path.join(temp_dir, "mytest.json")
-    mvol.write_json_file(json_file_full_path, {"x":1, "y":2})
+    mocal.write_json_file(json_file_full_path, {"x":1, "y":2})
 
-    for path in [mvol.volume_mount_dir, f"{mvol.volume_mount_dir}/test_get_FileEntry", f"{mvol.volume_mount_dir}/test_get_FileEntry", temp_dir, json_file_full_path]:
-        entry = mvol.get_FileEntry(path)
-        print(f"mvol.get_FileEntry({path})=", mvol.get_FileEntry(path), "\n\n")
+    for path in [mocal.volume_mount_dir, f"{mocal.volume_mount_dir}/test_get_FileEntry", f"{mocal.volume_mount_dir}/test_get_FileEntry", temp_dir, json_file_full_path]:
+        entry = mocal.get_FileEntry(path)
+        print(f"mocal.get_FileEntry({path})=", mocal.get_FileEntry(path), "\n\n")
         
         # entry.path will not have a leading slash, so remove from path before testing equality
         if path != "/" and path.startswith('/'): path=path.replace("/","",1)
         assert entry.path == path, f"Expected '{path}' but got '{entry.path}' from {entry}"
 
     for path in ["a", "mytest.json", "second_level_dir"]:
-        path_to_check = os.path.join(mvol.volume_mount_dir, path)
-        assert not mvol.get_FileEntry(full_path=path_to_check), f"Expected None but got {entry}"
+        path_to_check = os.path.join(mocal.volume_mount_dir, path)
+        assert not mocal.get_FileEntry(full_path=path_to_check), f"Expected None but got {entry}"
 
     # Remove the temp test dir
-    mvol.remove_file_or_directory(temp_dir)
+    mocal.remove_file_or_directory(temp_dir)
 
-@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mvol.volume})
+@app.function(image=image, volumes={MODAL_VOLUME_MOUNT_DIR: mocal.volume})
 def test_get_mtime():
     # Define our temp dir for this test and make sure it does not yet exist
     temp_dir = os.path.join(MODAL_VOLUME_MOUNT_DIR, "test_get_mtime_data")
-    if mvol.file_or_dir_exists(temp_dir) : mvol.remove_file_or_directory(temp_dir) # start fresh
+    if mocal.file_or_dir_exists(temp_dir) : mocal.remove_file_or_directory(temp_dir) # start fresh
 
     json_file_full_path = os.path.join(temp_dir, "test_get_mtime.json")
-    mvol.write_json_file(json_file_full_path, {"x":1})
+    mocal.write_json_file(json_file_full_path, {"x":1})
 
     now_in_seconds = int(datetime.now().timestamp())
     print(f"{now_in_seconds=}")
 
-    mtime = mvol.get_mtime(json_file_full_path)
+    mtime = mocal.get_mtime(json_file_full_path)
     print(f"         {mtime=}")
     assert abs(now_in_seconds - mtime) < 2
 
@@ -214,7 +214,7 @@ def test_get_mtime():
 def main():
     print("Running", __file__, "locally" if modal.is_local() else "remotely")
     
-    '''test_write_and_read_volume_json_file.local()
+    test_write_and_read_volume_json_file.local()
     test_write_and_read_volume_json_file.remote()
     test_create_or_remove_dir.local()
     test_create_or_remove_dir.remote()
@@ -227,6 +227,6 @@ def main():
     test_get_FileEntry.local()
     test_get_FileEntry.remote()
     test_get_mtime.local()
-    test_get_mtime.remote()'''
+    test_get_mtime.remote()
 
     
